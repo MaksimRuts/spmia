ebee62b4a6cae25815024cceb2539fd931411adb
Maksim Ruts <maksim.ruts@gmai.com>
2019-01-06 02:22:18 +0300

    created initial service

---
+++ licensing-service/src/main/java/com/roots/licenses/dto/License.java
@@ -0,0 +1,33 @@
+package com.roots.licenses.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class License {
+    private String licenseId;
+    private String productName;
+    private String licenseType;
+    private String organisationId;
+
+    public License withId(String licenseId) {
+        this.licenseId = licenseId;
+        return this;
+    }
+
+    public License withProductName(String productName) {
+        this.productName = productName;
+        return this;
+    }
+
+    public License withLicenseType(String licenseType) {
+        this.licenseType = licenseType;
+        return this;
+    }
+
+    public License withOrganisationId(String organisationId) {
+        this.organisationId = organisationId;
+        return this;
+    }
+}
